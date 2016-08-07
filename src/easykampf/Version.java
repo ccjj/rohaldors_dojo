25abfc55558989d0438bfd9ba460a774afb14b56
bob <christerus@gmx.de>
2016-08-07 14:33:05 +0200

    version updated

--- src/easykampf/Version.java
+++ src/easykampf/Version.java
@@ -11,7 +11,7 @@ package easykampf;
  * @author Omar
  */
 public class Version {
-    private static String version = "v 0.1a";
+    private static String version = "v 0.2";
     
     public static String getVersion(){
         return version;
